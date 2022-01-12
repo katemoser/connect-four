33851d0f101cb9991e5225eb502e2e10cd8247e8
Nate Snyder <n8.snyder@berkeley.edu>
2022-01-12 15:50:04 -0800

    handleClick checks for draw and switches players' turns

--- connect4.js
+++ connect4.js
@@ -108,10 +108,12 @@ function handleClick(evt) {
   }
 
   // check for tie
-  // TODO: check if all cells in board are filled; if so call, call endGame
-  
+  if (board[0].every(cell => cell !== null)) {
+    endGame("Game is a tie!");
+  }
+
   // switch players
-  // TODO: switch currPlayer 1 <-> 2
+  currPlayer = currPlayer === 1 ? 2 : 1;
 }
 
 /** checkForWin: check board cell-by-cell for "does a win start here?" */
